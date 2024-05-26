674243bad908894b7f28b169b6b8329824f397d2
IamMaheshGurung <162683026+IamMaheshGurung@users.noreply.github.com>
2024-05-26 14:39:18 +0900

    this much for today

---
+++ myPortFolio/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import AboutMe
+ from './components/AboutMe'
+function App() {
+  
+  return (
+    <>
+    <div>
+      <Header/>
+      <AboutMe/>
+    </div>
+      <h1> Happy World</h1>
+    </>
+  )
+}
+
+export default App
